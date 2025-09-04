808fbaf6f71e7729522bc8d9629e49e5ac0ac0bc
Aditya <aditya09bisht@gmail.com>
2025-09-04 22:49:00 +0530

    Time: 34 ms (76.55%) | Memory: 18.6 MB (53.26%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,43 +8,19 @@
  */
 class Solution {
 public:
-    //in this func t1->smaller ll t2->larger ll d=diff
-    ListNode* collPoint(ListNode* t1,ListNode* t2,int d)
-    {
-          while(d)
-          {
-            d--;
-            t2=t2->next;
-          }
-          while(t2)
-          {
-            if(t1==t2)
-            return t1;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* t1=headA;
+        ListNode* t2=headB;
+        if(headA==NULL || headB==NULL)
+        return NULL;
+        while(t1!=t2)
+        {
             t1=t1->next;
             t2=t2->next;
-          }
-          return nullptr;
-    }
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1=headA;
-        ListNode* temp2=headB;
-        int cnt1=1,cnt2=1;
-        while(temp1)
-        {
-            temp1=temp1->next;
-            cnt1++;
+             if(t1==t2) return t1;
+            if(t1==nullptr) t1=headB;
+            if(t2==nullptr) t2=headA;
         }
-        while(temp2)
-        {
-            temp2=temp2->next;
-            cnt2++;
-        }
-        if(cnt1>cnt2)
-        {
-            return collPoint(headB,headA,cnt1-cnt2);
-        }
-        else
-           return collPoint(headA,headB,cnt2-cnt1);
-
+        return t1;
     }
 };
