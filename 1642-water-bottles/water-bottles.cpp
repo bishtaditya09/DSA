9c702f52900ea6e4135f1a4fe98e58163ff35e74
Aditya <aditya09bisht@gmail.com>
2025-10-01 17:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (45.09%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int numWaterBottles(int num, int numEx) {
-      return num+(num-1)/(numEx-1);
+     int tot=num;
+     int emp=num; //kitni khali ho skti h
+     while(emp>=numEx)
+     {
+        int nb=emp/numEx;  //itni aur khali ho jaegi
+        tot+=nb;      
+        emp=emp%numEx+nb;     //isme modulo isiliye kiya taki remaining aa jaye
+     }
+     return tot;
     }
 };
