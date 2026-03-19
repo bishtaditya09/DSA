50dda1ebdd767100eb9a6a92e64f13ad81db06a4
Aditya <aditya09bisht@gmail.com>
2026-03-20 00:02:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.63%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int far=0,end=0;
+        int jm=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            far=max(far,i+nums[i]);
+            if(i==end)
+            {
+                jm++;
+                end=far;
+            }
+        }
+        return jm;
+    }
+};
