bb2e7c39cd3c753008e7ea2be29356ddd973a505
Aditya <aditya09bisht@gmail.com>
2026-04-23 13:13:31 +0530

    Time: 511 ms (49.24%) | Memory: 243.4 MB (33.68%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void merge(vector<int>&arr,int lo,int mid,int hi)
+    {
+    vector<int>temp;
+    int le=lo;
+    int ri=mid+1;
+    while(le<=mid && ri<=hi)
+    {
+        if(arr[ri]<arr[le])
+        {
+            temp.push_back(arr[ri]);
+            ri++;
+        }
+        else
+        {
+            temp.push_back(arr[le]);
+            le++;
+        }
+    }
+    while(le<=mid)
+    {
+        temp.push_back(arr[le]); 
+        le++;
+    }
+    while(ri<=hi)
+    {
+        temp.push_back(arr[ri]);
+        ri++;
+    }
+    for(int i=lo;i<=hi;i++)
+    {
+        arr[i]=temp[i-lo];
+    }
+}
+int mergesort(vector<int>&arr,int lo,int hi)
+{
+    int cnt=0;
+    if(lo>=hi) return 0;
+    int mid=(lo+hi)/2;
+    cnt+=mergesort(arr,lo,mid);
+    cnt+=mergesort(arr,mid+1,hi);
+     
+    int le=lo,ri=mid+1;
+    while(le<=mid && ri<=hi)
+    {
+        if(arr[le]>2ll*arr[ri])
+        {
+            cnt+=(mid-le+1);
+            ri++;
+        }
+        else
+        le++;
+    }
+    merge(arr,lo,mid,hi);
+    return cnt;
+}
+    int reversePairs(vector<int>&arr) {
+        return mergesort(arr,0,arr.size()-1);
+    }
+};
