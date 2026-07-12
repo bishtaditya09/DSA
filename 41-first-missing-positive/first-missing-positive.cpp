2d20fa99098042cc395e4169112e40e0facb61e0
Aditya <aditya09bisht@gmail.com>
2026-07-12 11:48:36 +0530

    Time: 55 ms (18.11%) | Memory: 78.3 MB (13.57%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        mp[nums[i]]=i;
+        for(int i=1;i<=n;i++)
+        {
+            if(mp.find(i)==mp.end())
+            {
+             return i;
+            }
+        }
+        return n+1;
+    }
+};
